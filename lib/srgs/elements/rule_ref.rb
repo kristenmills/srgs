808380b535d43642cfaa0f78ffb9591f24630e6c
kristenmills <kristen@kristen-mills.com>
2014-01-24 01:54:14 -0500

    Added ruleref class

---
+++ lib/srgs/elements/rule_ref.rb
@@ -0,0 +1,12 @@
+  module Srgs
+    class RuleRef
+
+      attr_accessor :uri, :xml_tag
+
+      def initialize(rule)
+        @uri = "##{rule.id}"
+        @xml_tag = 'ruleref'
+      end
+
+    end
+  end
