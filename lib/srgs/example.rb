ed3b3260d904dff176fca84ba00da87366470c51
kristenmills <kristen@kristen-mills.com>
2014-01-24 01:26:53 -0500

    Added example class

---
+++ lib/srgs/example.rb
@@ -0,0 +1,12 @@
+module Srgs
+  class Example
+
+    attr_accessor :text, :xml_tag
+
+    def initialize(text)
+      @text = text
+      @xml_tag = "example"
+    end
+
+  end
+end
--- lib/srgs/grammar.rb
+++ lib/srgs/grammar.rb
@@ -1,11 +1,12 @@
   module Srgs
     class Grammar
 
-      attr_accessor :root, :rules
+      attr_accessor :root, :rules, :xml_tag
 
       def initialize
         @root = ""
         @rules = Array.new
+        @xml_tag = "grammar"
       end
 
     end
