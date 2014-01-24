e820c151631c0c29c6f5aea9f0ea6415b3a4d9ce
kristenmills <kristen@kristen-mills.com>
2014-01-24 10:01:49 -0500

    updated grammar

--- lib/srgs/elements/grammar.rb
+++ lib/srgs/elements/grammar.rb
@@ -6,11 +6,14 @@ module Srgs
     def initialize(root = '')
       @root = root
       @lexicons = Array.new
+      @metas = Array.new
       @rules = Array.new
     end
 
-    def <<(rule)
-      @rules << rule
+    def <<(element)
+      @rules << element if element.is_a Rule
+      @lexicons << element if element is_a Lexicon
+      @metas << element if element is_a Meta
     end
 
   end
